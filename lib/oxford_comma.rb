5d1eb7a8578277f8253ed873341bfb06e40b5aa4
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-20 14:26:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
   count = 0
-  if array.count <= 2
+  if array.count == 3
     return array.join(" and ")
-    elsif array.count == 3
-    return array.join(","" and ")
+    elsif array.count
     else array.count
     return array.join(" and ")
 end
